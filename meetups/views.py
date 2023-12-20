f3bcfc18cfed7d1970df9b888e3044157302e205
David <davidmtobias@yahoo.es>
2023-12-20 18:15:50 +0100

    End Meetups

--- meetups/views.py
+++ meetups/views.py
@@ -1,6 +1,8 @@
 '''Descripción del módulo views in meetups'''
-from django.shortcuts import render
-from .models import Meetup
+from django.shortcuts import render, redirect
+
+from .models import Meetup, Participant
+from .forms import RegistrationForm
 
 # from django.http import HttpResponse
 
@@ -20,12 +22,31 @@ def meetup_details(request, meetup_slug):
     '''View Details'''
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                user_email = registration_form.cleaned_data['email']
+                participant, was_created = Participant.objects.get_or_create(email=user_email)
+                selected_meetup.participants.add(participant)
+                return redirect('confirm-registration', meetup_slug=meetup_slug)
+
+        
         return render(request, 'meetups/meetup-details.html', {
-            'meetup_found': True,
-            'meetup_title': selected_meetup.title,
-            'meetup_description': selected_meetup.description
-        })
+                'meetup_found': True,
+                'meetup': selected_meetup,
+                'form': registration_form
+            })
+
     except Exception as exc:
+        #raise exc
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found': False
         })
+
+def confirm_registration(request, meetup_slug):
+    meetup = Meetup.objects.get(slug=meetup_slug)
+    return render(request, 'meetups/registration-success.html', {
+        'organizer_email': meetup.organizer_email
+    })
